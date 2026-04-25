be8e76b3cf0aa50cc1ebdef15de52675fd430365
Mundigots <u1995402@campus.udg.edu>
2026-04-25 13:32:43 +0200

    S'ha afegit la opcio mida grup

--- js/options.js
+++ js/options.js
@@ -3,23 +3,32 @@ import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 var options = function(){
     const default_options = {
         pairs: 2,
-        difficulty: 'normal'
+        difficulty: 'normal',
+		groupSize: 2 // Mida dels grups per defecte
     } 
-
+	
+	// Variables
     var pairs = $('#pairs');
     var difficulty = $('#dif');
+	var groupSize = $('#groupSize'); 
     
     var savedOptions = localStorage.options && JSON.parse(localStorage.options);
     var options = Object.create(default_options);
-
+	
+	// Assignació de valors en funció de les condicions
     if (savedOptions && savedOptions.pairs)
         options.pairs = savedOptions.pairs;
     if (savedOptions && savedOptions.difficulty)
         options.difficulty = savedOptions.difficulty;
-
+	if (savedOptions && savedOptions.groupSize)
+		options.groupSize = savedOptions.groupSize;
+	
+	// Establir valors
     pairs.val(options.pairs);
     difficulty.val(options.difficulty);
-
+	groupSize.val(options.groupSize);
+	
+	// Guardar canvis
     pairs.on('change', function (){
         options.pairs = pairs.val();
     });
@@ -27,7 +36,11 @@ var options = function(){
     difficulty.on('change', function (){
         options.difficulty = difficulty.val();
     });
-
+	
+	groupSize.on('change', function (){
+		options.groupSize = groupSize.val();
+	});
+	
     return {
         applyChanges: function(){
             localStorage.options = JSON.stringify(options);
@@ -35,8 +48,11 @@ var options = function(){
         defaultValues: function(){
             options.pairs = default_options.pairs;
             options.difficulty = default_options.difficulty;
+			options.groupSize = default_options.groupSize;
+			
             pairs.val(options.pairs);
             difficulty.val(options.difficulty);
+			groupSize.val(options.groupSize);
         }
     }
 }();
